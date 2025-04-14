88bce42d3d73483ba2413a447c7c8d151776c3c7
azizulhoq953 <azizulhoq2041@gmail.com>
2025-04-14 14:54:44 +0600

    email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,7 +13,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://portfolioserver953.netlify.app/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
